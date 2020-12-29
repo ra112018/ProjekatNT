5f66436603bb5d68f94b2bc306a3de2bb83c8600
Natasa Vasic <vasic.ra10.2018@uns.ac.rs>
2020-12-29 10:26:28 +0100

    #pretraga_profesora Izmenjena pretraga profesora

--- src/controller/PretragaTable.java
+++ src/controller/PretragaTable.java
@@ -11,6 +11,7 @@ import javax.swing.JTable;
 import javax.swing.RowFilter;
 import javax.swing.table.TableRowSorter;
 
+import view.AbstractTableModelPredmeti;
 import view.AbstractTableModelProfesor;
 import view.AbstractTableModelStudent;
 import view.GlavniProzor;
@@ -19,9 +20,11 @@ public class PretragaTable {
 
 	private TableRowSorter<AbstractTableModelProfesor> rowSorterProfesor;
 	private TableRowSorter<AbstractTableModelStudent> rowSorterStudent;
+	private TableRowSorter<AbstractTableModelPredmeti> rowSorterPredmeti;
 
 	private JTable tabelaPretrage;
 	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public PretragaTable(JTable tabela, Integer kojaTabela) {
 		this.tabelaPretrage = tabela;
 		if(kojaTabela == 1) {
@@ -29,6 +32,8 @@ public class PretragaTable {
 		}
 		else if (kojaTabela == 0) {
 			rowSorterStudent =new TableRowSorter(tabelaPretrage.getModel());
+		}else if(kojaTabela == 2) {
+			rowSorterPredmeti = new TableRowSorter(tabelaPretrage.getModel());
 		}
 	}
 	
@@ -59,7 +64,29 @@ public class PretragaTable {
 				}
 			}
 			return map;
-		} 
+		} else if(tipPretr.equals("predmet")) {
+			String[] splitTackaZ = tekstPretrage.split(";");
+			String[] splitDvotacka = null;
+			for(int i = 0; i < splitTackaZ.length; i++) {
+				splitDvotacka = splitTackaZ[i].split(":");
+				if(splitDvotacka.length==2) {
+					if(splitDvotacka[1].trim().equals("")) {
+						map.clear();
+						return map;
+					}
+					if(splitDvotacka[0].toLowerCase().trim().equals("sifra predmeta")) {
+						map.put(0, splitDvotacka[1].trim());
+				}else {
+					if(splitDvotacka[0].trim().equals("") == false) {
+						map.clear();
+						return map;
+					}
+				}
+			}
+				
+		}
+			return map;
+		}
 			
 			else	return null;
 		}
@@ -70,6 +97,26 @@ public class PretragaTable {
 		
 		if(map.isEmpty() == false) {
 			tabelaPretrage.setRowSorter(rowSorterProfesor);
+			List<RowFilter<Object,Object>> filteri = new ArrayList<RowFilter<Object,Object>>(map.size());
+			
+			for(Entry<Integer,String> e : map.entrySet()) {
+				filteri.add(RowFilter.regexFilter("(?i)" + e.getValue(), e.getKey()));
+				
+			}
+			
+			RowFilter<Object,Object> servisFilter = RowFilter.andFilter(filteri);
+			rowSorterProfesor.setRowFilter(servisFilter);
+		}else {
+			JOptionPane.showMessageDialog(GlavniProzor.getInstance(), "Kriterijum za pretragu nije dobro unesen!");
+		}
+	}
+	
+	
+	public void pretragaPredmeta(String tekstPretrage) {
+		Map<Integer,String>  map = izdvoj("predmet", tekstPretrage);
+		
+		if(map.isEmpty() == false) {
+			tabelaPretrage.setRowSorter(rowSorterPredmeti);
 			List<RowFilter<Object,Object>> filteri = new ArrayList<RowFilter<Object,Object>>(map.size());
 			
 			for(Entry<Integer,String> e : map.entrySet()) {
